89ad1f16b7bf0075853ee2ac355dedd7d687fb89
tylersherrick <tyler.sherrick@gmail.com>
2024-08-22 15:44:42 +0000

    added nfl click functionality showing games

--- script/script.js
+++ script/script.js
@@ -31,7 +31,7 @@ const fetchAllSportsData = async () => {
 };
 
 const displayMainView = () => {
-    sportsDiv.innerHTML = `<h1>Active Sporting Events</h1>`;
+    sportsDiv.innerHTML = `<h1>Todays Sporting Events</h1>`;
     mlbData.innerHTML = `<h3 id="show-all-mlb">MLB</h3>`;
     const activeMLBGames = sportsData.MLB.filter(event => 
         event.status.type.name === "STATUS_IN_PROGRESS" || event.status.type.name === "STATUS_SCHEDULED"
@@ -55,26 +55,40 @@ const displayMainView = () => {
         mlbData.innerHTML += `<p>There are no games active or scheduled today</p>`;
     }
 
-    nflData.innerHTML = `<h3>NFL</h3>`;
+    nflData.innerHTML = `<h3 id="show-all-nfl">NFL</h3>`;
     const activeNFLGames = sportsData.NFL.filter(event => 
         event.status.type.name === "STATUS_IN_PROGRESS" || event.status.type.name === "STATUS_SCHEDULED"
     ).slice(0, 3);
     activeNFLGames.forEach(event => {
+        const gameDate = event.date;
+        const dateObject = new Date(gameDate);
+        const options = { timeZone: 'America/Chicago', year: 'numeric', month: '2-digit', day: '2-digit' };
+        const adjustedDate = dateObject.toLocaleDateString('en-CA', options);
+        const today = new Date().toISOString().split('T')[0];
         const awayTeam = event.competitions[0].competitors[1].team.displayName;
         const homeTeam = event.competitions[0].competitors[0].team.displayName;
         const awayScore = event.competitions[0].competitors[1].score;
         const homeScore = event.competitions[0].competitors[0].score;
         const inning = event.status.type.detail;
-        nflData.innerHTML += `
-            <p>${awayTeam} ${awayScore} at ${homeTeam} ${homeScore} - ${inning}</p>
-        `;
+        if(today === adjustedDate && event.status.type.name === "STATUS_SCHEDULED") {
+            nflData.innerHTML += `
+                <p>${awayTeam} at ${homeTeam} - ${inning}</p>
+            `;
+        }
+        if(today === adjustedDate && event.status.type.name === "STATUS_IN_PROGRESS") {
+            nflData.innerHTML += `
+                <p>${awayTeam} ${awayScore} at ${homeTeam} ${homeScore} - ${inning}</p>
+            `;
+        }
     });
+    
     if(activeNFLGames.length === 0) {
         nflData.innerHTML += `<p>There are no games active or scheduled today</p>`;
     }
+
     nhlData.innerHTML = `<h3>NHL</h3>`;
     const activeNHLGames = sportsData.NHL.filter(event => 
-        event.status.type.name === "STATUS_IN_PROGRESS" || event.status.type.name === "STATUS_SCHEDULED"
+        event.status.type.name === "STATUS_IN_PROGRESS"
     ).slice(0, 3);
     activeNHLGames.forEach(event => {
         const awayTeam = event.competitions[0].competitors[1].team.displayName;
@@ -90,6 +104,7 @@ const displayMainView = () => {
         nhlData.innerHTML += `<p>There are no games active or scheduled today</p>`;
     }
     document.getElementById('show-all-mlb').addEventListener('click', displayMLBSection);
+    document.getElementById('show-all-nfl').addEventListener('click', displayNFLSection);
 };
 
 const displayMLBSection = () => {
@@ -117,6 +132,32 @@ const displayMLBSection = () => {
     });
     document.getElementById('back-to-main').addEventListener('click', displayMainView);
 };
+
+const displayNFLSection = () => {
+    mlbData.innerHTML = "";
+    nhlData.innerHTML = "";
+    sportsDiv.innerHTML = '<h1>NFL Games This Week</h1>';
+    nflData.innerHTML = '<button id="back-to-main">Back</button>';
+    sportsData.NFL.forEach(event => {
+        const awayTeam = event.competitions[0].competitors[1].team.displayName;
+        const homeTeam = event.competitions[0].competitors[0].team.displayName;
+        const awayScore = event.competitions[0].competitors[1].score;
+        const homeScore = event.competitions[0].competitors[0].score;
+        const inning = event.status.type.detail;
+        const outs = event.competitions[0].outsText;
+        const gameStatus = event.status.type.shortDetail;
+        if(event.status.type.name === "STATUS_IN_PROGRESS") {
+            nflData.innerHTML += `<p>${awayTeam} ${awayScore} at ${homeTeam} ${homeScore} - ${inning} - ${outs}</p>`;
+        }
+        if(event.status.type.name === "STATUS_SCHEDULED") {
+            nflData.innerHTML += `<p>${awayTeam} at ${homeTeam} - ${inning} ${gameStatus}</p>`;
+        }
+        if(event.status.type.name === "STATUS_FINAL") {
+            nflData.innerHTML += `<p>${awayTeam} ${awayScore} at ${homeTeam} ${homeScore} - ${inning}</p>`;
+        }
+    });
+    document.getElementById('back-to-main').addEventListener('click', displayMainView);
+}
 
 fetchAllSportsData().then(displayMainView);
 setInterval(fetchAllSportsData, 6000);
