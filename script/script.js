46081dd973c2777e99067d79a720d6e1c472e20c
tylersherrick <tyler.sherrick@gmail.com>
2025-07-29 13:49:56 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -52,6 +52,7 @@ const fetchGamesData = async () => {
 const updateViews = () => {
     if (currentView === 'showLess') {
         showLessMLB();
+        showLessNFL();
     } 
     if (currentView === 'mlb') {
         showAllMLB();
@@ -83,7 +84,7 @@ const showLessMLB = () => {
     mlbData.innerHTML = ``;
     const inProgress = sportsData.MLB.slice(0, sportsData.MLB.length).filter(event => event.status.type.name === "STATUS_IN_PROGRESS");
     const yetToStart = sportsData.MLB.slice(0, sportsData.MLB.length).filter(event => event.status.type.name === "STATUS_SCHEDULED");
-    const limitedGames = [...inProgress, ...yetToStart].slice(0, 6).filter(event => 
+    const limitedGames = [...inProgress, ...yetToStart].slice(0, 3).filter(event => 
         event.status.type.name === "STATUS_IN_PROGRESS" || event.status.type.name === "STATUS_SCHEDULED"
     );
     const gameTotal = limitedGames.length;
