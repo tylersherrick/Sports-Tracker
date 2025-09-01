657c7a20ce65fe817a3f081774f3fdf06072a8a6
tylersherrick <tyler.sherrick@gmail.com>
2025-08-31 21:51:41 -0500

    updated script and cfb files
    updated script to look into adding posession arrow for nfl and cfb.
    updated cfbvariables to show similar views for all of the views. will need to wait until another ranked in progress game to test the other views.

--- script/cfbVariables.js
+++ script/cfbVariables.js
@@ -19,6 +19,7 @@ function cfbVariables(event) {
         situation: event.competitions[0].situation || "",
         ballPosition: event.competitions[0].situation?.downDistanceText || 'Switching Possession',
         posession: event.competitions[0].situation?.possession || "",
+        
         scheduledGame() {
             return `
                 <div id="${this.gameId}" class="game-row scheduled">
@@ -49,53 +50,35 @@ function cfbVariables(event) {
         },
         inProgress() {
             return `
-                <div class="game-row">
-                    <div class="game-info">
-                        <p class="game-details">
-                            ${this.time} </br></br>
-                            ${this.awayRank} ${this.awayTeam} -  ${this.awayScore} ${this.awayID}</br>
-                            ${this.homeRank} ${this.homeTeam} -  ${this.homeScore} ${this.homeID}</br></br>
-                        </p>
-                        <p class="game-details">
-                            ${this.allPosition}
-                        </p>
+                <div id="${this.gameId}" class="game-row scheduled">
+                    <!-- Away team: logo, score, abbrev -->
+                    <div class="team away">
+                        <img src="${this.awayLogo}" class="team-logo" alt="${this.awayTeam}">
+                        <div class="team-info">
+                            <div class="score">${this.awayRank}</div>
+                            <div class="abbr">${this.shortAwayTeam}</div>
+                        </div>
+                    </div>
+
+                    <!-- Center: inning/short detail -->
+                    <div class="game-center">
+                        <div class="inning-outs">
+                            ${this.scheduleTime}
+                        </div>
+                    </div>
+
+                    <!-- Home team: score, abbrev, logo -->
+                    <div class="team home">
+                        <div class="team-info">
+                            <div class="score">${this.homeRank}</div>
+                            <div class="abbr">${this.shortHomeTeam}</div>
+                        </div>
+                        <img src="${this.homeLogo}" class="team-logo" alt="${this.homeTeam}">
                     </div>
                 </div>
             `
         },
         endOfPeriod() {
-            return `
-                <div class="game-row">
-                    <div class="game-info">
-                        <p class="game-details">
-                            ${this.time} </br></br>
-                            ${this.awayRank} ${this.awayTeam} -  ${this.awayScore} </br>
-                            ${this.homeRank} ${this.homeTeam} -  ${this.homeScore} </br></br>
-                        </p>
-                        <p class="game-details">
-                            End of Quarter
-                        </p>
-                    </div>
-                </div>
-            `
-        },
-        halfTime() {
-            return `
-                <div class="game-row">
-                    <div class="game-info">
-                        <p class="game-details">
-                            ${this.time} </br></br>
-                            ${this.awayRank} ${this.awayTeam} -  ${this.awayScore} </br>
-                            ${this.homeRank} ${this.homeTeam} -  ${this.homeScore} </br></br>
-                        </p>
-                        <p class="game-details">
-                            Halftime
-                        </p>
-                    </div>
-                </div>
-            `
-        },
-        gameOver() {
             return `
                 <div id="${this.gameId}" class="game-row over">
                     <div class="team away">
@@ -111,6 +94,53 @@ function cfbVariables(event) {
                     <div class="team home">
                         <div class="team-info">
                             <div class="score">${this.homeScore}</div>
+                            <div class="abbr">${this.shortHomeTeam}</div>
+                        </div>
+                        <img src="${this.homeLogo}" class="team-logo" alt="${this.homeTeam}">
+                    </div>
+                </div>
+            `
+        },
+        halfTime() {
+            return `
+                <div id="${this.gameId}" class="game-row over">
+                    <div class="team away">
+                        <img src="${this.awayLogo}" class="team-logo" alt="${this.awayTeam}">
+                        <div class="team-info">
+                            <div class="score">${this.awayScore}</div>
+                            <div class="abbr">${this.shortAwayTeam}</div>
+                        </div>
+                    </div>
+                    <div class="game-center">
+                        <div class="inning-outs">${this.scheduleTime}</div>
+                    </div>
+                    <div class="team home">
+                        <div class="team-info">
+                            <div class="score">${this.homeScore}</div>
+                            <div class="abbr">${this.shortHomeTeam}</div>
+                        </div>
+                        <img src="${this.homeLogo}" class="team-logo" alt="${this.homeTeam}">
+                    </div>
+                </div>
+            `
+        },
+        gameOver() {
+            return `
+                <div id="${this.gameId}" class="game-row over">
+                    <div class="team away">
+                        <img src="${this.awayLogo}" class="team-logo" alt="${this.awayTeam}">
+                        <div class="team-info">
+                            <div class="score">${this.homeRank}</div>
+                            <div class="abbr">${this.shortAwayTeam}</div>
+                        </div>
+                    </div>
+                    <div class="game-center">
+                        <div class="inning-outs">${this.scheduleTime}</div>
+                        <div class="score">${this.awayScore} - ${this.homeScore}</div>
+                    </div>
+                    <div class="team home">
+                        <div class="team-info">
+                            <div class="score">${this.awayRank}</div>
                             <div class="abbr">${this.shortHomeTeam}</div>
                         </div>
                         <img src="${this.homeLogo}" class="team-logo" alt="${this.homeTeam}">
