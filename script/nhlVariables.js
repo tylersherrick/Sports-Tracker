ad04415e3189ce2e4dd4f8f3c949aef3a346b9d6
tylersherrick <tyler.sherrick@gmail.com>
2025-09-22 10:30:58 -0500

    updated nhlVariables.js
    updated this file to include a few new variables. These include the venue, team odds, and records. Now the three game status views for individuals work and look the same as the other leagues that are live.

--- script/nhlVariables.js
+++ script/nhlVariables.js
@@ -1,6 +1,7 @@
 function nhlVariables(event) {
     return {
         gameId: event.id,
+        attendance: event.competitions[0].attendance,
         awayTeam: event.competitions[0].competitors[1].team.displayName,
         homeTeam: event.competitions[0].competitors[0].team.displayName,
         awayLogo: event.competitions[0].competitors[1].team.logo,
@@ -11,6 +12,13 @@ function nhlVariables(event) {
         shortHomeTeam: event.competitions[0].competitors[0].team.abbreviation,
         time: event.status.type.detail,
         scheduleTime: event.status.type.shortDetail,
+        gameStatus: event.status.type.description,
+        awayRecord: event.competitions[0].competitors[1].records[0].summary,
+        homeRecord: event.competitions[0].competitors[0].records[0].summary,
+        venue: event.competitions[0].venue.fullName,
+        spread: event.competitions[0].odds?.[0]?.details || "",
+        awayOdds: event.competitions[0].odds?.[0]?.awayTeamOdds?.moneyLine || "",
+        homeOdds: event.competitions[0].odds?.[0]?.awayTeamOdds?.moneyLine || "",
         inProgress() {
             return `
                 <div id="${this.gameId}" class="game-row scheduled">
@@ -102,5 +110,117 @@ function nhlVariables(event) {
                 </div>
             `
         },
+        renderIndividualView() {
+            if(this.homeOdds > 0) {
+                this.homeOdds = `+${this.homeOdds}`;
+            }
+            if(this.awayOdds > 0) {
+                this.awayOdds = `+${this.awayOdds}`;
+            }
+            if (this.gameStatus === "Scheduled") {
+                return `
+                    <div id="${this.gameId}" class="game-row individual-game-row">
+                        <div class="team away individual-away">
+                            <img src="${this.awayLogo}" class="individual-logo" alt="${this.awayTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortAwayTeam} - ${this.awayRecord}</div>
+                                <div class="abbr individual-details">${this.awayOdds}</div>
+                            </div>
+                        </div>
+                        <div class="individual-center">
+                            <div class="center-item">${this.awayTeam} at ${this.homeTeam}</div>
+                            <div class="center-item">${this.venue}</div>
+                            <div class="center-item">${this.scheduleTime}</div>
+                            <div class="center-item">${this.spread}</div>
+                        </div>
+                        <div class="team home">
+                            <img src="${this.homeLogo}" class="individual-logo" alt="${this.homeTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortHomeTeam} - ${this.homeRecord}</div>
+                                <div class="abbr individual-details">${this.homeOdds}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+            if (this.gameStatus === "In Progress") {
+                return `
+                    <div id="${this.gameId}" class="game-row individual-game-row">
+                        <div class="team away individual-away">
+                            <img src="${this.awayLogo}" class="individual-logo" alt="${this.awayTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortAwayTeam} - ${this.awayRecord}</div>
+                            </div>
+                        </div>
+                        <div class="individual-center">
+                            <div class="center-item">${this.awayTeam} at ${this.homeTeam}</div>
+                            <div class="center-item">${this.time}</div>
+                            <div class="center-item">${this.awayScore} - ${this.homeScore}</div>
+                        </div>
+                        <div class="team home">
+                            <img src="${this.homeLogo}" class="individual-logo" alt="${this.homeTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortHomeTeam} - ${this.homeRecord}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+            if (this.gameStatus === "End of Period") {
+                return `
+                    <div id="${this.gameId}" class="game-row individual-game-row">
+                        <div class="team away individual-away">
+                            <img src="${this.awayLogo}" class="individual-logo" alt="${this.awayTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortAwayTeam} - ${this.awayRecord}</div>
+                            </div>
+                        </div>
+                        <div class="individual-center">
+                            <div class="center-item">${this.awayTeam} at ${this.homeTeam}</div>
+                            <div class="center-item">${this.scheduleTime}</div>
+                            <div class="center-item">${this.awayScore} - ${this.homeScore}</div>
+                        </div>
+                        <div class="team home">
+                            <img src="${this.homeLogo}" class="individual-logo" alt="${this.homeTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortHomeTeam} - ${this.homeRecord}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+            if (this.gameStatus === "Final") {
+                return `
+                    <div id="${this.gameId}" class="game-row individual-game-row">
+                        <div class="team away individual-away">
+                            <img src="${this.awayLogo}" class="individual-logo" alt="${this.awayTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortAwayTeam} - ${this.awayRecord}</div>
+                            </div>
+                        </div>
+                        <div class="individual-center">
+                            <div class="center-item">${this.awayTeam} at ${this.homeTeam}</div>
+                            <div class="center-item"><strong>${this.scheduleTime}</strong>: ${this.awayScore} - ${this.homeScore}</div>
+                            <div class="center-item"><strong>Attendance</strong>: ${this.attendance}</div>
+                        </div>
+                        <div class="team home">
+                            <img src="${this.homeLogo}" class="individual-logo" alt="${this.homeTeam}">
+                            <div class="individual-team-info">
+                                <div class="score"></div>
+                                <div class="abbr individual-details">${this.shortHomeTeam} - ${this.homeRecord}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+            return "Game info unavailable.";
+        }
     }
 }
